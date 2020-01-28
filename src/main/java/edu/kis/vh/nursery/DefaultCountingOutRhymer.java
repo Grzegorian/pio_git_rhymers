de02082c2e25c33b955c44da55aae51928a84650
grzegorian <grzegorzpiasta@gmail.com>
2020-01-28 14:04:49 +0100

    2.1zrobione

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,39 +1,33 @@
 package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
-
-    private final int MAX_SIZE = 12;
-    private final int EMPTY_RHYMER_INDICATOR = -1;
-    private final int DEFAULT_VALUE = -1;
-
-
-    private final int[] NUMBERS = new int[MAX_SIZE];
-
-    public int total = EMPTY_RHYMER_INDICATOR;
-
-    public void countIn(int in) {
-        if (!isFull())
-            NUMBERS[++total] = in;
-    }
-
-    public boolean callCheck() {
-        return total == EMPTY_RHYMER_INDICATOR;
-    }
-
-    public boolean isFull() {
-        return total == MAX_SIZE-1;
-    }
-
-    protected int peekaBoo() {
-        if (callCheck())
-            return DEFAULT_VALUE;
-        return NUMBERS[total];
-    }
-
-    public int countOut() {
-        if (callCheck())
-            return DEFAULT_VALUE;
-        return NUMBERS[total--];
-    }
-
+	IntArrayStack intArrayStack;
+	
+	public DefaultCountingOutRhymer() {
+		this.intArrayStack = new IntArrayStack();
+	}
+	
+	public DefaultCountingOutRhymer(IntArrayStack intArrayStack) {
+		this.intArrayStack = intArrayStack;
+	}
+	
+	public void countIn(int in) {
+		intArrayStack.countIn(in);
+	}
+	
+	public boolean callCheck() {
+		return intArrayStack.callCheck();
+	}
+	
+	public boolean isFull() {
+		return intArrayStack.isFull();
+	}
+	
+	public int peekaBoo() {
+		return intArrayStack.peekaBoo();
+	}
+	
+	public int countOut() {
+		return intArrayStack.countOut();
+	}
 }
